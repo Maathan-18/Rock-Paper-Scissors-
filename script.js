1425ffa4f7d44eb8dcec95bf9dd526143d99f4d4
Peter <96875781+Maathan-18@users.noreply.github.com>
2022-10-29 12:56:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+var you;
+var yourScore=0;
+var opponent;
+var opponentScore=0;
+const result_p=document.querySelector(".result > p")
+
+var choices=["rock","paper","scissors"];
+
+window.onload=function(){
+    for(let i=0;i<3;i++)
+    {
+        let choice=document.createElement("img");
+        choice.id=choices[i];
+        choice.src=choices[i]+ ".png";
+        choice.addEventListener("click",selectChoice);
+        document.getElementById("choices").append(choice);
+    }
+}
+function selectChoice(){
+    you=this.id;
+    document.getElementById("your-choice").src=you + ".png";
+     
+    opponent=choices[Math.floor(Math.random()*3)];
+    document.getElementById("opponent-choice").src=opponent+ ".png";
+    
+    
+
+    if(you==opponent)
+    {
+        yourScore+=0;
+        opponentScore+=0;
+    }
+    else
+    {
+        if(you=="rock")
+        {
+            if(opponent=="scissors")
+            yourScore+=1;
+            
+            else if(opponent=="paper")
+            opponentScore+=1;
+                
+
+
+        }
+        else if(you=="paper")
+        {
+            if(opponent=="rock")
+           yourScore+=1;
+                
+            else if(opponent=="scissors")
+            opponentScore+=1;
+                
+            
+        }
+        else if(you=="scissors")
+        {
+            if(opponent=="paper")
+            yourScore+=1;
+                
+            else if(opponent=="rock")
+            opponentScore+=1;
+                
+            
+        }
+    
+}
+
+    document.getElementById("your-score").innerText=yourScore + " YOU ";
+    document.getElementById("opponent-score").innerText=opponentScore+" COMP ";
+}
